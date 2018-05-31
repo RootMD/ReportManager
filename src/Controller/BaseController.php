ac15b59b4bf8480fdb46207837442ce11895c24f
Michał Rutkowski <Fosternmr@gmail.com>
2018-05-31 17:08:53 +0200

    added twigs

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -9,10 +9,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class BaseController
+class BaseController extends AbstractController
 {
     /**
      * @Route("/")
@@ -27,9 +28,16 @@ class BaseController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'bla %s',
-            $slug
-        ));
+
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('Commits/show.html.twig', [
+            'title'=> ucwords(str_replace('-','',$slug)),
+            'comments' => $comments,
+            ]);
     }
 }
